6afb0925adc10cd81343f9826afbf439f1b08e4b
Markus Ineichen <markus_ineichen@gmx.ch>
2017-02-15 14:11:26 +0100

    Add documentation

--- website/src/Controller/IndexController.php
+++ website/src/Controller/IndexController.php
@@ -4,15 +4,25 @@ namespace ihrname\Controller;
 
 use ihrname\SimpleTemplateEngine;
 
-class IndexController {
+class IndexController 
+{
+  /**
+   * @var ihrname\SimpleTemplateEngine Template engines to render output
+   */
   private $template;
+  
+  /**
+   * @param ihrname\SimpleTemplateEngine
+   */
   public function __construct(SimpleTemplateEngine $template)
   {
      $this->template = $template;
   }
+
   public function homepage() {
     echo "INDEX";
   }
+
   public function greet($name) {
   	echo $this->template->render("hello.html.php", ["name" => $name]);
   }
--- website/src/SimpleTemplateEngine.php
+++ website/src/SimpleTemplateEngine.php
@@ -2,12 +2,29 @@
 
 namespace ihrname;
 
-class SimpleTemplateEngine {
+/**
+ * Simple Template-Engine which provides arguments into a template,
+ * captures the output and returns it to the caller.
+ */
+class SimpleTemplateEngine 
+{
+  /**
+   * @var string Location of the directory containing the templates
+   */
   private $templatePath;
+  
+  /**
+   * @param string $templatePath 
+   */
   public function __construct($templatePath) 
   {
     $this->templatePath = $templatePath;
   }
+  
+  /**
+   * Renders a *.html.php file inside the template path
+   * @return string
+   */
   public function render($template, array $arguments = []) 
   {
     extract($arguments);
