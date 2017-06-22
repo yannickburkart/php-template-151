2647fce026e8d6fab651debf91b1a7ecf17dc7e6
yannickburkart <yannick.burkart@gmx.ch>
2017-06-22 09:04:42 +0200

    a

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
-# This scripts changes the volumes to be writable for the www-data user
-DOCKER_PREFIX=phptemplate151
+docker-compose run --rm -u root fpm sh -c "chown -R 82 .."
+docker.compose run --rm fpm composer install
+docker-compose down
 
-#82 is the user-id of www-data inside the container
-docker run -v ${DOCKER_PREFIX}_upload:/upload --rm alpine sh -c "chown -R 82 /upload"
