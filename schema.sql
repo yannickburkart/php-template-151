4a8c427edbba271f9056e0c145aec7beef7a87c4
yannickburkart <yannick.burkart@gmx.ch>
2017-06-22 09:20:28 +0200

    sql

--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-CREATE DATABASE `app` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
 USE `app`;
 
 DROP TABLE IF EXISTS `user`;
